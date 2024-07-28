029cc3e1407b7ffc9f091594a96e5a457e12cf25
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-07-28 11:48:16 +0800

    wip: make it accept arrays

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export function renderJsTemplateString(
   template: string,
-  kv: Record<string, string>,
+  kv: { [key: string | number]: string },
 ): string {
   return template.replace(/\$\{\w+\}/g, (str: string) => {
     const key = str.slice(2, -1);
