44d0060342449dc2aef3c90ad093f11f52cdd122
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-09-21 23:10:16 +0800

    feat: match and replace input with regex

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Bookmarks with URL templates
 # todo
 
 - [x] automatically fill `templateInput` with user's clipboard content
+- [x] match and replace input with regex and captured groups
 - [ ] introduce tailwindcss
 - [ ] design a URL template scheme
 - [ ] add an `input:file` to accept user's URL templates
--- src/make-links.ts
+++ src/make-links.ts
@@ -1,18 +1,21 @@
-import { renderJsTemplateString } from "./utils";
+import { matchAndReplace, renderJsTemplateString } from "./utils";
 
-export type TemplateType = string | { match: string; target: string };
+export type TemplateType =
+  | string
+  | { predicate: string; match: string; replace: string };
 export type LinkRenderer = (ctx: { input: string }) => string | null;
 
 export default function (template: TemplateType): LinkRenderer {
   if (typeof template === "string") {
     return (ctx) => renderJsTemplateString(template, ctx);
-  } else if (typeof template.match === "string") {
+  } else if (typeof template.predicate === "string") {
     return (ctx) => {
-      const match = ctx.input.match(template.match);
-      if (match === null) {
-        return null;
+      const regex = new RegExp(template.predicate);
+      const shouldApplyThisTemplate = null !== ctx.input.match(regex);
+      if (shouldApplyThisTemplate) {
+        return matchAndReplace(ctx.input, template.match, template.replace);
       } else {
-        return renderJsTemplateString(template.target, match);
+        return null;
       }
     };
   }
